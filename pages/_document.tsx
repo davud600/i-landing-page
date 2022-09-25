3f336c02570eb04561dd4ceb79d954d6a17b78b9
davud <davudgorqi@gmail.com>
2022-09-25 22:32:21 +0200

    importing stylesheets on globals.css and scripts are next/script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,11 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
     return (
         <Html>
             <Head>
-                <link href='/static/img/favicon.png' rel='icon'></link>
+                {/* <link href='/static/img/favicon.png' rel='icon'></link>
                 <link
                     href='/static/img/apple-touch-icon.png'
                     rel='apple-touch-icon'></link>
@@ -18,7 +19,7 @@ export default function Document() {
                     crossOrigin={"true"}></link>
                 <link
                     href='https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,600;1,700&family=Roboto:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Work+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap'
-                    rel='stylesheet'></link>
+                    rel='stylesheet'></link> */}
 
                 {/* <link
                     href='/static/vendor/bootstrap/css/bootstrap.min.css'
@@ -26,7 +27,7 @@ export default function Document() {
                 <link
                     href='/static/vendor/bootstrap-icons/bootstrap-icons.css'
                     rel='stylesheet'></link> */}
-                <link
+                {/* <link
                     href='/static/vendor/fontawesome-free/css/all.min.css'
                     rel='stylesheet'></link>
                 <link href='/static/vendor/aos/aos.css' rel='stylesheet'></link>
@@ -37,7 +38,7 @@ export default function Document() {
                     href='/static/vendor/swiper/swiper-bundle.min.css'
                     rel='stylesheet'></link>
 
-                <link href='/static/css/main.css' rel='stylesheet'></link>
+                <link href='/static/css/main.css' rel='stylesheet'></link> */}
             </Head>
             <body>
                 <Main />
@@ -51,15 +52,31 @@ export default function Document() {
 
                 <div id='preloader'></div>
 
-                <script src='/static/vendor/bootstrap/js/bootstrap.bundle.min.js'></script>
-                <script src='/static/vendor/aos/aos.js'></script>
-                <script src='/static/vendor/glightbox/js/glightbox.min.js'></script>
-                <script src='/static/vendor/isotope-layout/isotope.pkgd.min.js'></script>
-                <script src='/static/vendor/swiper/swiper-bundle.min.js'></script>
-                <script src='/static/vendor/purecounter/purecounter_vanilla.js'></script>
-                <script src='/static/vendor/php-email-form/validate.js'></script>
+                <Script
+                    strategy='beforeInteractive'
+                    src='/static/vendor/bootstrap/js/bootstrap.bundle.min.js'></Script>
+                <Script
+                    strategy='beforeInteractive'
+                    src='/static/vendor/aos/aos.js'></Script>
+                <Script
+                    strategy='beforeInteractive'
+                    src='/static/vendor/glightbox/js/glightbox.min.js'></Script>
+                <Script
+                    strategy='beforeInteractive'
+                    src='/static/vendor/isotope-layout/isotope.pkgd.min.js'></Script>
+                <Script
+                    strategy='beforeInteractive'
+                    src='/static/vendor/swiper/swiper-bundle.min.js'></Script>
+                <Script
+                    strategy='beforeInteractive'
+                    src='/static/vendor/purecounter/purecounter_vanilla.js'></Script>
+                <Script
+                    strategy='beforeInteractive'
+                    src='/static/vendor/php-email-form/validate.js'></Script>
 
-                <script src='/static/js/main.js'></script>
+                <Script
+                    strategy='beforeInteractive'
+                    src='/static/js/main.js'></Script>
             </body>
         </Html>
     );
